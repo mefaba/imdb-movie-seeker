2e67af10e21d0642683273b1481af4fe50a127ba
mefa <mefaba@outlook.com>
2021-05-25 12:38:42 -0400

    update folder structure

--- src/components/Forms/PickForMeForm.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PickForMeForm = () => {
-  return <div>PickForMeForm</div>;
-};
-
-export default PickForMeForm;
--- src/components/Forms/index.js
+++
@@ -1,3 +0,0 @@
-export { default as HeardMovieForm } from "./HeardMovieForm.js";
-export { default as NoChanceForm } from "./NoChanceForm.js";
-export { default as PickForMeForm } from "./PickForMeForm.js";
